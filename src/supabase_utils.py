417be6f87821df8590f28d2e4b26fcee3b6fbd68
rhyni <98338673+rhyni@users.noreply.github.com>
2026-04-07 14:33:50 +0700

    Update supabase_utils.py

--- src/supabase_utils.py
+++ src/supabase_utils.py
@@ -1,1 +1,12 @@
+from supabase import create_client
+from kaggle_secrets import UserSecretsClient
 
+def get_client():
+    user_secrets = UserSecretsClient()
+    supabase_url = user_secrets.get_secret("SUPABASE_URL")
+    supabase_key = user_secrets.get_secret("SUPABASE_KEY")
+
+    if not supabase_url or not supabase_key:
+        raise ValueError("Thiếu SUPABASE_URL hoặc SUPABASE_KEY trong Kaggle Secrets.")
+
+    return create_client(supabase_url, supabase_key)
