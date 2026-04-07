b4e7a3e096a06d603579e8bb5c522e45141fac17
rhyni <98338673+rhyni@users.noreply.github.com>
2026-04-07 16:37:14 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -26,4 +26,4 @@ file_path = "/kaggle/working/retail-recommender-system/src/config.py"
 with open(file_path, "w", encoding="utf-8") as f:
     f.write(config_code)
 
-print("Đã ghi lại config.py")
+print("written:", file_path)
